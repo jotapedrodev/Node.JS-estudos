e9e2c92890974ed72179a2ea3c106cd73c7142a6
Joao Pedro <jotinhadib@gmail.com>
2024-12-05 14:24:12 -0300

    funcao verificar certa

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ const fs = require('fs');
 const caminhoArquivo = process.argv;
 const link = caminhoArquivo[2];
 fs.readFile(link, 'utf-8', (error, texto) => {
+    verificaPalavaraDuplicada(texto);
 });
 
 function verificaPalavaraDuplicada(texto){
     const listaPalavra = texto.split(' ')
+    const resultado = {};
     listaPalavra.forEach(palavra => {
         resultado[palavra] = (resultado[palavra] || 0)+ 1
     })
 console.log (resultado);
-}
+};
 
