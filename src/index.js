20832e86a2f17e160f8cb1d27ea459ebe5ecc614
Joao Pedro <jotinhadib@gmail.com>
2024-12-05 14:20:34 -0300

    funcao verificarplvraduplicada

--- src/index.js
+++ src/index.js
@@ -1,19 +1,15 @@
-// Importa o módulo 'fs' (file system) do Node.js, usado para trabalhar com arquivos.
+
 const fs = require('fs');
-
-// 'process.argv' é um array que contém os argumentos passados para o script via terminal.
-// Aqui pegamos todos os argumentos.
 const caminhoArquivo = process.argv;
-
-// Pegamos o terceiro argumento (índice 2), que será o caminho do arquivo a ser lido.
 const link = caminhoArquivo[2];
-
-//    - 'error': contém informações do erro, caso aconteça.
-//    - 'texto': contém o conteúdo do arquivo se a leitura for bem-sucedida.
 fs.readFile(link, 'utf-8', (error, texto) => {
-    // Mostra o conteúdo do arquivo no console.
-    console.log(texto);
 });
 
-// Exibe no console o valor de 'link' (o caminho do arquivo passado).
-console.log(link);
+function verificaPalavaraDuplicada(texto){
+    const listaPalavra = texto.split(' ')
+    listaPalavra.forEach(palavra => {
+        resultado[palavra] = (resultado[palavra] || 0)+ 1
+    })
+console.log (resultado);
+}
+
